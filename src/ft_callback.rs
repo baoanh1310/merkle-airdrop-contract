06485d80453a785a76167a796c9b3e760402f69b
baoanh1310 <dataholicguy@gmail.com>
2022-05-28 00:33:26 +0700

    merge

--- src/ft_callback.rs
+++ src/ft_callback.rs
@@ -1,32 +1,27 @@
 use crate::*;
-use near_contract_standards::fungible_token::metadata::FungibleTokenMetadata;
 
 const GAS_FOR_FT_TRANSFER: Gas = Gas(15_000_000_000_000);
 const XCC_GAS: Gas = Gas(20_000_000_000_000);
 
 #[ext_contract(ext_self)]
-pub trait ExtSelf {
-    fn callback_decimal(&self) -> u128;
-}
-
-pub trait ExtSelf {
+trait ExtSelf {
     fn callback_decimal(&self) -> u128;
 }
 
 #[ext_contract(ext_ft)]
-pub trait FungibleTokenCore {
+pub trait FungibleToken {
     fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>);
 }
 
 #[ext_contract(ext_ft_metadata)]
-pub trait FungibleTokenMeta {
-	fn ft_metadata(&self) -> FungibleTokenMetadata;
+pub trait FungibleTokenMetadataProvider {
+    fn ft_metadata(&self) -> FungibleTokenMetadata;
 }
 
 #[near_bindgen]
 impl Contract {
-    pub fn claim_token(&self, airdrop_id: AirdropId, account_id: AccountId, amount: U128) {
-    	ext_ft_metadata::ft_metadata(
+    pub fn get_ft_decimals(&self, airdrop_id: AirdropId) -> Promise {
+        ext_ft_metadata::ft_metadata(
             self.get_ft_contract_by_campaign(airdrop_id),
             1,
             GAS_FOR_FT_TRANSFER,
@@ -36,21 +31,10 @@ impl Contract {
             0,
             XCC_GAS,
         ))
-        // ext_ft::ft_transfer(
-        //     account_id.clone(),
-        //     amount,
-        //     None,
-        //     self.get_ft_contract_by_campaign(airdrop_id),
-        //     1,
-        //     GAS_FOR_FT_TRANSFER,
-        // );
     }
-}
 
-#[near_bindgen]
-impl ExtSelf for Contract {
-	#[private]
-    fn callback_decimal(&self) -> u128 {
+    #[private]
+    pub fn callback_decimal(&self) -> u128 {
         assert_eq!(env::promise_results_count(), 1, "ERR_TOO_MANY_RESULTS");
 
         match env::promise_result(0) {
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
+use hex;
+use near_contract_standards::fungible_token::metadata::FungibleTokenMetadata;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
+use near_sdk::json_types::U128;
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{
+    env, ext_contract, near_bindgen, AccountId, Balance, Gas,
+    PanicOnDefault, Promise, PromiseResult,
+};
+use std::convert::TryFrom;
+
 use crate::ft_callback::*;
 use crate::internal::*;
 use crate::utils::*;
 use crate::view::*;
-use hex;
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
-use near_sdk::json_types::{Base64VecU8, U128};
-use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, ext_contract, near_bindgen, AccountId, Balance, Gas, Promise, PromiseResult, PanicOnDefault};
-use std::convert::{TryFrom, TryInto};
 
 mod ft_callback;
 mod internal;
@@ -76,7 +81,7 @@ impl Contract {
 
         self.internal_add_account_to_claimed_list(&airdrop_id);
 
-        self.claim_token(airdrop_id, user_id, U128(amount));
+        // self.claim_token(airdrop_id, user_id, U128(amount));
     }
 }
 
